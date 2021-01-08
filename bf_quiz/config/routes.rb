3f4e64cb488a9473d32f1d8d923fad22611b3851
nimisaya <nimisaya@outlook.com>
2021-01-08 18:28:24 +1100

    Add user and session routes

--- bf_quiz/config/routes.rb
+++ bf_quiz/config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  # Create routes for users
+  # users     GET    /users(.:format)          users#index
+  #           POST   /users(.:format)          users#create
+  # new_user  GET    /users/new(.:format)      users#new
+  # edit_user GET    /users/:id/edit(.:format) users#edit
+  # user      GET    /users/:id(.:format)      users#show
+  #           PATCH  /users/:id(.:format)      users#update
+  #           PUT    /users/:id(.:format)      users#update
+  #           DELETE /users/:id(.:format)      users#destroy
+  resources :users
+
+  # Login/logout routes
+  get '/login'  => 'session#new'        # showing the login form
+  post '/login' => 'session#create'     # form submits to here, performs login, redirects
+  delete '/login' => 'session#destroy'  # logout link goes here, perform logout, redirect
+
 end
