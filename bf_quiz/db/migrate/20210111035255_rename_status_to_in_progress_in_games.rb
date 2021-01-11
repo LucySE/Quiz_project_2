38c056c3404f52afe623afccebe84864cf204c2f
nimisaya <nimisaya@outlook.com>
2021-01-11 16:43:19 +1100

    Remove duplicate migrations

--- bf_quiz/db/migrate/20210111035255_rename_status_to_in_progress_in_games.rb
+++
@@ -1,5 +0,0 @@
-class RenameStatusToInProgressInGames < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :games, :status, :in_progress
-  end
-end
--- bf_quiz/db/migrate/20210111035549_remove_puzzle_id_from_games.rb
+++
@@ -1,5 +0,0 @@
-class RemovePuzzleIdFromGames < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :games, :puzzle_id
-  end
-end
