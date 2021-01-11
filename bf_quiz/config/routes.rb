b110154169484a1b60e11e3538228ab2837ba076
nimisaya <nimisaya@outlook.com>
2021-01-11 22:16:52 +1100

    Rename win page to gameover

--- bf_quiz/config/routes.rb
+++ bf_quiz/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # # Root path
   root to: 'pages#home'
-  get '/win' => 'pages#win'
+  get '/gameover' => 'pages#gameover'
   get '/lose' => 'pages#lose'
   post '/'  => 'pages#play'
   post '/users/:id'  => 'pages#play'
