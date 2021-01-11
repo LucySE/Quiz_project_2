3167126438cde2e1efcddc5fcb70f7651b1e41d6
nimisaya <nimisaya@outlook.com>
2021-01-11 22:41:34 +1100

    Add comments and questions to routes

--- bf_quiz/config/routes.rb
+++ bf_quiz/config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  # # Root path
+  # Root path
   root to: 'pages#home'
-  get '/gameover' => 'pages#gameover'
+
+  # WHAT IS THIS ROUTE FOR? What is the play page?
   post '/'  => 'pages#play'
-  post '/users/:id'  => 'pages#play'
+  post '/users/:id'  => 'pages#play' 
   get '/' => 'users#show'
 
-  # Create routes for users
+  # Create routes for users to create accounts, login and view/edit/delete their profile
   # users     GET    /users(.:format)          users#index
   #           POST   /users(.:format)          users#create
   # new_user  GET    /users/new(.:format)      users#new
@@ -23,11 +24,29 @@ Rails.application.routes.draw do
   post '/login' => 'session#create'     # form submits to here, performs login, redirects
   delete '/login' => 'session#destroy'  # logout link goes here, perform logout, redirect
 
-  # Create routes for puzzles
-
+  # Create routes for puzzles - DO WE NEED ALL OF THESE ROUTES?
+  #   puzzles GET    /puzzles(.:format)          puzzles#index
+  #   POST   /puzzles(.:format)          puzzles#create
+  # new_puzzle GET    /puzzles/new(.:format)      puzzles#new
+  # edit_puzzle GET    /puzzles/:id/edit(.:format) puzzles#edit
+  # puzzle GET    /puzzles/:id(.:format)      puzzles#show
+  #   PATCH  /puzzles/:id(.:format)      puzzles#update
+  #   PUT    /puzzles/:id(.:format)      puzzles#update
+  #   DELETE /puzzles/:id(.:format)      puzzles#destroy
   resources :puzzles
 
-  # Create routes for games
+  # Create routes for games - DO WE NEED ALL OF THESE ROUTES?
+  #   games   GET    /games(.:format)            games#index
+  #           POST   /games(.:format)            games#create
+  # new_game  GET    /games/new(.:format)        games#new
+  # edit_game GET    /games/:id/edit(.:format)   games#edit
+  # game      GET    /games/:id(.:format)        games#show
+  #           PATCH  /games/:id(.:format)        games#update
+  #           PUT    /games/:id(.:format)        games#update
+  #           DELETE /games/:id(.:format)        games#destroy
   resources :games
+
+    #  Create route for end of game
+    get '/gameover' => 'pages#gameover'
 
 end
